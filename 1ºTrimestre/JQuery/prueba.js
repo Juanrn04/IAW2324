27ec27e46cf4a8dd65954f008361c17c610acc0a
Juanrn04 <juanrodriguez04@iesamachado.org>
2023-11-15 12:02:26 +0100

    Añadido

--- 1ºTrimestre/JQuery/prueba.js
+++ 1ºTrimestre/JQuery/prueba.js
@@ -3,20 +3,40 @@
 $(document).ready(function () {
 
     $("#esconder").click(function () { 
-        $("#tercero").fadeOut(2000);
+        var entrada = prompt("Por favor, cual quieres esconder:");
+
+        if(entrada=="todos")
+        {
+            $("p").hide();
+        }
+        else
+        {
+            $("#" + entrada).hide();
+        }
+
+        
         
     });
     $("#mostrar").click(function () { 
-       $("#tercero").fadeIn(2000);
+        var variable = prompt("Por favor, cual quieres mostrar:");
+
+        if(variable=="todos")
+        {
+            $("p").show();
+        }
+        else
+        {
+            $("#" + variable).show();
+        }
+        
         
     });
 
     $("#alternar").click(function () { 
-         $("#tercero").empty();
+     alert($("#campo").val("hola"));
         
     });
 
-    mous
 
 
 });

90e4cd82441ed3d59f53c88f411e723b9f8a6562
Juanrn04 <juanrodriguez04@iesamachado.org>
2023-11-15 10:50:21 +0100

    Añadido

--- 1ºTrimestre/JQuery/prueba.js
+++ 1ºTrimestre/JQuery/prueba.js
@@ -1,7 +1,22 @@
 
 
 $(document).ready(function () {
-    
+
+    $("#esconder").click(function () { 
+        $("#tercero").fadeOut(2000);
+        
+    });
+    $("#mostrar").click(function () { 
+       $("#tercero").fadeIn(2000);
+        
+    });
+
+    $("#alternar").click(function () { 
+         $("#tercero").empty();
+        
+    });
+
+    mous
 
 
 });

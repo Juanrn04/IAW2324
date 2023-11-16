5d04c3580e9e6dabfca48e22a609f9cd9beab4b5
juanrn04 <juanrodriguez04@iesamachado.org>
2023-11-16 16:12:49 +0100

    Añadido

--- 1ºTrimestre/JQuery/ej3.js
+++ 1ºTrimestre/JQuery/ej3.js
@@ -1,16 +1,22 @@
 
 
 $(document).ready(function () {
-    var tamaño=20;
+    var tamaño= 16;
     $("#aumentar").click(function() { 
         tamaño = tamaño + 1;
-        $(".pares").css("font-size",tamaño);
+        $(".pares").css({
+            "font-size":tamaño ,
+            "color":"green"});
+
+        
         
     });
 
     $("#disminuir").click(function() { 
         tamaño = tamaño - 1;
-        $(".pares").css("font-size",tamaño);
+        $(".pares").css({
+        "font-size":tamaño ,
+        "color":"red"});
     });
 });
 

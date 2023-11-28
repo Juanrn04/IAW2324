3df820b7d202900799c7e3fc889e3d2e31f30599
Juanrn04 <juanrodriguez04@iesamachado.org>
2023-11-28 14:28:33 +0100

    Añadido

---
+++ 1ºTrimestre/PHP/fechahora.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fecha y hora</title>
+</head>
+<body>
+
+<?php
+
+
+
+setlocale(LC_ALL,"es_ES");
+echo strftime("%A %d de %B del %Y a las %H:%M");
+
+
+?>
+
+
+</body>
+</html>

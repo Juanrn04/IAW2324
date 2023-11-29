b11d6e40fe4a62740d0401e099f086aa669ce268
juanrn04 <juanrodriguez04@iesamachado.org>
2023-11-29 20:50:31 +0100

    Añadido

--- 1ºTrimestre/PHP/foreach3.php
+++ 1ºTrimestre/PHP/foreach3.php
@@ -3,22 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>For each 1</title>
+    <title>For each 3</title>
 </head>
 <body>
 <?php
 
-$refranes = array("hola ","adios ","buenas ","que tal ","hasta luego ");
-
-foreach ($refranes as $refran) 
-{
-    echo "<p>$refran</p>" ;
-}
 
 
 
 
 
 ?>
+
+
+
 </body>
 </html>

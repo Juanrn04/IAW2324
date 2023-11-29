6993d23d8e7b598aa78358633ed91061ea8ba15f
Juanrn04 <juanrodriguez04@iesamachado.org>
2023-11-29 12:31:17 +0100

    Añadido

---
+++ 1ºTrimestre/PHP/foreach1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>For each 1</title>
+</head>
+<body>
+<?php
+
+$refranes = array[hola,adios,buenas,que tal,hasta luego]
+
+foreach ($refranes as $v) {
+    echo $v;
+}
+
+
+
+
+?>
+</body>
+</html>

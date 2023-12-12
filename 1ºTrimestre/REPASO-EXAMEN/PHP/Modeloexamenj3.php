66dfc389b331a742df630f66c4c60cb0c4ca9b0e
Juanrn04 <juanrodriguez04@iesamachado.org>
2023-12-12 10:52:39 +0100

    Añadido

---
+++ 1ºTrimestre/REPASO-EXAMEN/PHP/Modeloexamenj3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Correo 
+    </title>
+</head>
+<body>
+    <h1>Escriba un correo</h1>
+<form method="get">
+    <input type="text" name="asunto" placeholder="Asunto"><br><br>
+    <input type="text" name="destinatario" placeholder="Destinatario" ><br><br>
+    <input type="submit" value="Enviar">
+
+</form>
+    
+</body>
+</html>

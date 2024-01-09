f9f35002906843870e0a0f5003348d3b23c9f0ac
juanrn04 <juanrodriguez04@iesamachado.org>
2024-01-09 21:01:18 +0100

    Añadido

--- 2ºTrimestre/PHP/MYSQL/gestion/conexion.php
+++ 2ºTrimestre/PHP/MYSQL/gestion/conexion.php
@@ -2,6 +2,7 @@
 $servername = "sql307.thsite.top";
 $username = "thsi_35748575";
 $password = "uS9rA5MT";
+$dbname = "thsi_35748575_bdprueba";
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=thsi_35748575_bdprueba", $username, $password);

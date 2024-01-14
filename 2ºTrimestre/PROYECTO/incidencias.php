de18c42daa4ab0149bd578c9cd566b95a8b76bf9
juanrn04 <juanrodriguez04@iesamachado.org>
2024-01-14 14:20:39 +0100

    Añadido

--- 2ºTrimestre/PROYECTO/incidencias.php
+++ 2ºTrimestre/PROYECTO/incidencias.php
@@ -1,18 +1,52 @@
 <?php
-  include "conexion.php";
-        
-  header("Content-type:text/html;charset=utf-8");
+
+
+  $servername = "sql307.thsite.top";
+  $username = "thsi_35748575";
+  $password = "uS9rA5MT";
+  $dbname = "thsi_35748575_bdprueba";
+  
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  $conn->set_charset("utf8mb4");
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
 
 $sql = "SELECT * FROM incidencias";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Planta: " . $row["planta"]. " -Aula: " . $row["aula"]. "- Descripcion: " . $row["descripcion"]. " - Alta: " . $row["alta"]. " - Revision: " . $row["revision"]. "- Resolucion: " . $row["resolucion"]. " - Comentario: " . $row["comentario"]. "<br>";
-  }
+echo "<table border='1'>
+<tr>
+<th>Id</th>
+<th>Planta</th>
+<th>Aula</th>
+<th>Descripcion</th>
+<th>Alta</th>
+<th>Revision</th>
+<th>Resolucion</th>
+<th>Comentario</th>
+</tr>";
+
+while($row = $result->fetch_assoc()) {
+    echo "<tr>";
+    echo "<td>" . $row['id'] . "</td>";
+    echo "<td>" . $row['planta'] . "</td>";
+    echo "<td>" . $row['aula'] . "</td>";
+    echo "<td>" . $row['descripcion'] . "</td>";
+    echo "<td>" . $row['alta'] . "</td>";
+    echo "<td>" . $row['revision'] . "</td>";
+    echo "<td>" . $row['resolucion'] . "</td>";
+    echo "<td>" . $row['comentario'] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+
 } else {
   echo "0 results";
 }
+
 $conn->close();
 ?>

d784a0d3f58f907fae8e5da674d532a0c621b5d6
juanrn04 <juanrodriguez04@iesamachado.org>
2024-01-14 14:35:48 +0100

    Select de incidencias

--- 2ºTrimestre/PROYECTO/incidencias.php
+++ 2ºTrimestre/PROYECTO/incidencias.php
@@ -1,22 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Bienvenido</title>
+</head>
+<body>
+    <h2>Bienvenido, <?php echo $_SESSION['usuario']; ?>!</h2>
+</body>
+</html>
+
+
 <?php
 
+/*Conexion a la base de datos de PEPE
+include "conexion.php";
+header("Content-type:text/html;charset=utf-8"); */
 
-  $servername = "sql307.thsite.top";
-  $username = "thsi_35748575";
-  $password = "uS9rA5MT";
-  $dbname = "thsi_35748575_bdprueba";
+//Conexion a la base de datos de JUAN
+$servername = "sql307.thsite.top";
+$username = "thsi_35748575";
+$password = "uS9rA5MT";
+$dbname = "thsi_35748575_bdprueba";
   
-  // Create connection
-  $conn = new mysqli($servername, $username, $password, $dbname);
-  $conn->set_charset("utf8mb4");
-  // Check connection
-  if ($conn->connect_error) {
+// creamos la conexion
+$conn = new mysqli($servername, $username, $password, $dbname);
+$conn->set_charset("utf8mb4");
+// Confirmamos que la conexión se ha realizado de forma correcta
+if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-  }
+}
 
+//Operación a realizar
 $sql = "SELECT * FROM incidencias";
 $result = $conn->query($sql);
 
+//Estrutura de la tabla
 if ($result->num_rows > 0) {
 echo "<table border='1'>
 <tr>
